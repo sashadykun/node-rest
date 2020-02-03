372341691667c70dc517d01fa9cf724e2d171afa
SD <sashaluckyman@gmail.com>
2020-02-02 22:39:43 -0800

    nodemon

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('hello world')
 });
 
+app.get('/api/courses', (req, res) => {
+    res.send([1, 2, 3]);
+})
+
 app.listen( 4000, () => console.log('Listening on port 4000... ') )
 
 // app.post();
